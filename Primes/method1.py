a4ed381904bca55c367cf2bf817682861a479813
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-01 16:14:34 -0400

    Update method1.py

--- Primes/method1.py
+++ Primes/method1.py
@@ -3,10 +3,10 @@
 # (Method 1)
 
 def check_prime(num):
-  for i in range(2, int(num**0.5)):
-    if num%i==0:
-      return False
-  return True
+    for i in range(2, int(num**0.5)+1):
+        if num%i==0:
+            return False
+    return True
 
 number = int(input())
 
